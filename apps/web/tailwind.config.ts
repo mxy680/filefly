89d04b17a914c12d22819dc993a3672d036c7815
Mark Shteyn <markshteyn1@gmail.com>
2024-10-15 19:16:45 -0400

    web reinitialization

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,19 +1,9 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import sharedConfig from "@repo/tailwind-config"
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+const config: Pick<Config, "content" | "presets"> = {
+  content: ["./src/app/**/*.tsx"],
+  presets: [sharedConfig],
+}
+
+export default config
