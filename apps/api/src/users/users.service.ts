d38c320dc0189720aea6d37bdc9322326e7cc26f
Mark Shteyn <markshteyn1@gmail.com>
2024-10-16 16:01:00 -0400

    removed nest

--- apps/api/src/users/users.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/db/db.service';
-import { User, Prisma } from '@prisma/client';
-
-@Injectable()
-export class UsersService {
-    constructor(private prisma: PrismaService) { }
-}
